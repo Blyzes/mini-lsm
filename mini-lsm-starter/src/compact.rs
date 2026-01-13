81222ae3aab1b2971ee8235526f37fd0020133c0
Blyzes <mengqz08@gamil.com>
2026-01-13 19:12:40 +0800

    week3_day4

--- mini-lsm-starter/src/compact.rs
+++ mini-lsm-starter/src/compact.rs
@@ -139,6 +139,8 @@ impl LsmStorageInner {
         // Prevents empty SSTs and enables take() for ownership transfer.
         let mut builder = None;
         let mut last_key = Vec::<u8>::new();
+        let watermark = self.mvcc().watermark();
+        let mut first_key_below_watermark = false;
 
         while iter.is_valid() {
             if builder.is_none() {
@@ -147,14 +149,33 @@ impl LsmStorageInner {
 
             let same_as_last_key = iter.key().key_ref() == last_key;
 
+            if !same_as_last_key {
+                first_key_below_watermark = true;
+            }
+
+            // Skip tombstones below watermark when compacting to bottom level.
+            if compact_to_bottom_level
+                && !same_as_last_key
+                && iter.key().ts() <= watermark
+                && iter.value().is_empty()
+            {
+                last_key.clear();
+                last_key.extend(iter.key().key_ref());
+                iter.next()?;
+                first_key_below_watermark = false;
+                continue;
+            }
+
+            // Skip keys below watermark except for the first key when there are multiple versions.
+            if same_as_last_key && iter.key().ts() <= watermark {
+                if !first_key_below_watermark {
+                    iter.next()?;
+                    continue;
+                }
+                first_key_below_watermark = false
+            }
+
             let builder_inner = builder.as_mut().unwrap();
-            // if compact_to_bottom_level {
-            //     if !iter.value().is_empty() {
-            //         builder_inner.add(iter.key(), iter.value());
-            //     }
-            // } else {
-            //     builder_inner.add(iter.key(), iter.value());
-            // }
 
             if builder_inner.estimated_size() >= self.options.target_sst_size && !same_as_last_key {
                 let sst_id = self.next_sst_id();
